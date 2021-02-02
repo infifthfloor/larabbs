fae306595d84ce5b856625d91a7e10d934f04c05
ouche <oyshengxu@163.com>
2021-02-02 23:19:35 +0800

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
+  @yield('styles')
+
 </head>
 
 <body>
@@ -34,6 +36,10 @@
 
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
+
+  @yield('scripts')
+
 </body>
 
 </html>
