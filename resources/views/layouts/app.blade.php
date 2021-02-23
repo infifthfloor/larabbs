b8bbefb81aa4aaa0112157dcf575bb30dbe8b1b7
ouche <oyshengxu@163.com>
2021-02-23 22:19:41 +0800

    站点权限部署

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,10 @@
     @include('layouts._footer')
   </div>
 
+  @if (app()->isLocal())
+    @include('sudosu::user-selector')
+  @endif
+
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
   <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
