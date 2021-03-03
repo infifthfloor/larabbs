067533ab3d848b6ea8354bca35c055038016673f
ouche <oyshengxu@163.com>
2021-03-03 23:56:04 +0800

    用户最后活跃时间

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ use Auth;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
+    use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
     use HasRoles;
     use HasFactory, MustVerifyEmailTrait;
